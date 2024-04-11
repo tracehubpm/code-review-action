2b1aa0a358f35774725b4fc963f61236dd71b152
h1alexbel <hialexbel@gmail.com>
2024-04-11 10:43:05 +0300

    feat(#60): Model.java, WithAnalyzed.java

---
+++ src/test/java/git/tracehub/codereview/action/deep/AnalyzedWithTest.java
@@ -0,0 +1,55 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2024 Tracehub.git
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package git.tracehub.codereview.action.deep;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test case for {@link AnalyzedWith}.
+ *
+ * @since 0.0.0
+ */
+final class AnalyzedWithTest {
+
+    @Test
+    void tagsResponse() throws Exception {
+        final String tagged = new AnalyzedWith(
+            new FakeModel(), "fake-gpt-4"
+        ).asString();
+        final String expected =
+            "This is a fake completion performed by"
+            + " FakeModel.java\n\nAnalyzed with fake-gpt-4";
+        MatcherAssert.assertThat(
+            String.format(
+                "Tagged response (%s) does not match with expected (%s)",
+                tagged,
+                expected
+            ),
+            tagged,
+            new IsEqual<>(expected)
+        );
+    }
+}
